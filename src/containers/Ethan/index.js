2d016b91d7d10bfa5a9d47ffce5bc7674b3c0229
ebaird492 <ebaird492@gmail.com>
2021-04-05 09:33:25 -0600

    Same background size?

--- src/containers/Ethan/index.js
+++ src/containers/Ethan/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import './index.css'
 
 class Ethan extends Component {
     render() {
         return (
-            <h>
-                Hello Python World!
-            </h>
+            <div className="Python">
+                <h>
+                    Hello Python World!
+                </h>
+            </div>
         )
     }
 }

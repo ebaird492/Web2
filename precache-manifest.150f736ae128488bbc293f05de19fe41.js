e249c1de0809f70851364ecd48af6918188c540b
ebaird492 <ebaird492@gmail.com>
2021-04-09 12:17:32 -0600

    Updates

---
+++ precache-manifest.150f736ae128488bbc293f05de19fe41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7341f2e2e69c8772cbdd8564da61781",
+    "url": "/Web2/index.html"
+  },
+  {
+    "revision": "069f68abe2a5a5c8e1bb",
+    "url": "/Web2/static/css/main.aea7fff4.chunk.css"
+  },
+  {
+    "revision": "1cf5f15d87cb0260e915",
+    "url": "/Web2/static/js/2.e17aa960.chunk.js"
+  },
+  {
+    "revision": "069f68abe2a5a5c8e1bb",
+    "url": "/Web2/static/js/main.d630ad58.chunk.js"
+  },
+  {
+    "revision": "85958784ebda794bf9f1",
+    "url": "/Web2/static/js/runtime~main.a8300c3d.js"
+  },
+  {
+    "revision": "e65ccfd904540204e6587f8a14c938b0",
+    "url": "/Web2/static/media/colorado.e65ccfd9.png"
+  },
+  {
+    "revision": "39d260bd62ac82b06cb4be40d4b253c7",
+    "url": "/Web2/static/media/woodback.39d260bd.png"
+  }
+]);
